618801dd6e30c2751d07b867e0e40e1d7d7fa0ee
richak13 <rkhed@seas.upenn.edu>
2024-10-21 23:54:30 -0400

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -17,9 +17,9 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 	#YOUR CODE HERE	
-  url = f"https://ipfs.io/ipfs/{cid}"
-  response = requests.get(url)
-  data = response.json()
+  	url = f"https://ipfs.io/ipfs/{cid}"
+ 	 response = requests.get(url)
+  	data = response.json()
 
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
 	return data
