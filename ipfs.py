0ff736f209a7d7b73117a4c4fc4e2a6303842037
richak13 <rkhed@seas.upenn.edu>
2024-10-26 17:06:55 -0400

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -5,7 +5,7 @@ def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
 	json_data = json.dumps(data)
-	url = "https://ipfs.io/api/v0/add"
+	url = "https://ipfs.infura.io:5001/api/v0/add"
 	files = {
 			'file': ('data.json', json_data)
 	}
